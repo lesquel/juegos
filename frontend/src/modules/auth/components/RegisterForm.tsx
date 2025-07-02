18f4fa705066f5a9a59226ac81cc5c1148b98b07
marco <marcoadrianzambra@gmail.com>
2025-07-02 16:38:53 -0500

    fix(login and register styles)

--- frontend/src/modules/auth/components/RegisterForm.tsx
+++ frontend/src/modules/auth/components/RegisterForm.tsx
@@ -51,12 +51,12 @@ const UseRegisterForm = () => {
 
   return (
     <div className="w-full px-4 max-w-sm sm:max-w-md mx-auto">
-      <h1 className="text-3xl lg:text-4xl font-bold mb-6 mt-4 text-center">
+      <h1 className="text-3xl font-bold mb-6 text-center">
         ¡Regístrate!
       </h1>
 
 
-      <div className="relative w-36 h-36 rounded-full bg-green-500 flex items-center justify-center overflow-hidden mb-8 mx-auto">
+      <div className="relative w-36 h-36 rounded-full bg-green-500 flex items-center justify-center overflow-hidden mb-3 mx-auto">
         <img
           src="/placeholder-avatar-welder.png"
           alt="Avatar ese tambien que estaba en figma"
@@ -64,15 +64,13 @@ const UseRegisterForm = () => {
         />
       </div>
 
-
-            <p className="text-sm text-gray-500 mb-6 border-b border-gray-300 pb-1 text-center">
-              ¿Ya tienes una cuenta?{" "}
-              <a href={authRoutesConfig.children.login.url} className="text-blue-600 hover:underline">
-                Inicia sesión
-              </a>
-            </p>
-      
-
+      <p className="text-sm text-gray-500 mb-3 border-b border-gray-300 pb-1 text-center">
+        ¿Ya tienes una cuenta?{" "}
+        <a href={authRoutesConfig.children.login.url} className="text-blue-600 hover:underline">
+          Inicia sesión
+        </a>
+      </p>
+    
       <form
         onSubmit={(e) => {
           e.preventDefault();
@@ -87,7 +85,7 @@ const UseRegisterForm = () => {
             <div className="mb-6">
               <label
                 htmlFor={field.name}
-                className="block text-lg font-medium text-gray-800 mb-3"
+                className="block text-base font-medium text-gray-800 mb-3"
               >
                 Ingresa tu nuevo usuario
               </label>
@@ -99,7 +97,7 @@ const UseRegisterForm = () => {
                 onChange={(e) => field.handleChange(e.target.value)}
                 onBlur={field.handleBlur}
                 placeholder="Ingresa tu correo electrónico"
-                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-base"
+                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-sm"
               />
               {field.state.meta.errors.length > 0 && (
                 <span className="text-red-500 text-sm mt-2 block">
@@ -117,7 +115,7 @@ const UseRegisterForm = () => {
             <div className="mb-6">
               <label
                 htmlFor={field.name}
-                className="block text-lg font-medium text-gray-800 mb-3"
+                className="block text-base font-medium text-gray-800 mb-3"
               >
                 Ingresa tu nueva contraseña
               </label>
@@ -129,7 +127,7 @@ const UseRegisterForm = () => {
                 onChange={(e) => field.handleChange(e.target.value)}
                 onBlur={field.handleBlur}
                 placeholder="Ingresa tu contraseña"
-                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-base"
+                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-sm"
               />
               {field.state.meta.errors.length > 0 && (
                 <span className="text-red-500 text-sm mt-2 block">
@@ -144,10 +142,10 @@ const UseRegisterForm = () => {
         <form.Field
           name="confirmPassword"
           children={(field) => (
-            <div className="mb-8">
+            <div className="mb-6">
               <label
                 htmlFor={field.name}
-                className="block text-lg font-medium text-gray-800 mb-3"
+                className="block text-base font-medium text-gray-800 mb-3"
               >
                 Confirma tu contraseña
               </label>
@@ -159,7 +157,7 @@ const UseRegisterForm = () => {
                 onChange={(e) => field.handleChange(e.target.value)}
                 onBlur={field.handleBlur}
                 placeholder="Confirma tu contraseña"
-                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-base"
+                className="w-full px-4 py-3 rounded-xl bg-gray-200 text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out text-sm"
               />
               {field.state.meta.errors.length > 0 && (
                 <span className="text-red-500 text-sm mt-2 block">
@@ -174,7 +172,7 @@ const UseRegisterForm = () => {
 
         <button
           type="submit"
-          className="w-full bg-gray-800 text-white font-semibold py-3 lg:py-4 rounded-xl shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 transition duration-300 ease-in-out text-base lg:text-xl cursor-pointer"
+          className="w-full bg-gray-800 text-white font-semibold py-3 rounded-xl shadow-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 transition duration-300 ease-in-out text-base cursor-pointer"
         >
           Regístrate!
         </button>
