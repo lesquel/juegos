a0a29711603b3407bd44aa723568888168b09bfd
lesquel <lesquel1319@gmail.com>
2025-07-19 11:50:13 -0500

    feat: mejorar lógica de categorías para mostrar juegos según la categoría seleccionada

--- frontend/src/modules/category-game/components/SingleCategoryGame.tsx
+++ frontend/src/modules/category-game/components/SingleCategoryGame.tsx
@@ -1,6 +1,7 @@
 import { QueryProvider } from "@providers/QueryProvider";
 import { CategoryGameClientData } from "../services/categoryGameClientData";
 import { LoadingComponent } from "@components/LoadingComponent";
+import { ListTagsGames } from "@modules/games/components/tags/ListTagsGames";
 
 export const SingleCategoryGame = ({ id }: { id: string }) => {
   return (
@@ -20,7 +21,7 @@ const UseSingleCategoryGame = ({ id }: { id: string }) => {
       <h1>{data?.category_name}</h1>
       <img src={data?.category_img} alt={data?.category_name} />
       <p>{data?.category_description}</p>
-      <p>Status: {data?.status ? "true" : "false"}</p>
+      <ListTagsGames categoryId={data?.category_id as string} />
     </div>
   );
 };
