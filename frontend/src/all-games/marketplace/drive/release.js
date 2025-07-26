8880d851838ea4e877372d2960f68acb35c33767
lesquel <lesquel1319@gmail.com>
2025-07-26 14:35:35 -0500

    feat: add DadosComponent to the marketplace page with layout integration

--- frontend/src/all-games/marketplace/drive/release.js
+++
@@ -1,9 +0,0 @@
-let debug, downloadLink, debugMesh, debugTile, debugTakeScreenshot;
-let showMap, debugGenerativeCanvas, debugInfo;
-
-function debugInit() {}
-function drawDebug() {}
-function debugSaveCanvas() {}
-function debugSaveText() {}
-function debugSaveDataURL() {}
-function ASSERT() {}
