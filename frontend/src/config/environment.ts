2ca0ff34dd135cfcd0b733f476f226df14823037
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-26 21:49:20 -0500

    .

--- frontend/src/config/environment.ts
+++ frontend/src/config/environment.ts
@@ -1,13 +1,3 @@
-const API_URL_DEV = import.meta.env.VITE_API_URL_DEV;
-const API_URL_PROD = import.meta.env.VITE_API_URL_PROD;
-
-if (!API_URL_DEV || !API_URL_PROD) {
-  throw new Error("Environment variables VITE_API_URL_DEV and VITE_API_URL_PROD must be set.");
-}
-
-export const API_URL =
-  import.meta.env.ENVIRONMENT === "development" ? API_URL_DEV : API_URL_PROD;
-
 export const environment = {
-  BASE_URL: import.meta.env.VITE_BASE_URL || "http://localhost:8000",
-};
+    BASE_URL: import.meta.env.VITE_BASE_URL || "http://localhost:8000",
+}
