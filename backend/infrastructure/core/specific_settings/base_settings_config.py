f684e9eb9d7d8b9e8c6b1531558f8b9372927b5b
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-07-26 19:21:42 -0500

    fix: fixed error when loading envs

--- backend/infrastructure/core/specific_settings/base_settings_config.py
+++ backend/infrastructure/core/specific_settings/base_settings_config.py
@@ -13,7 +13,9 @@ class BaseSettingsConfig(BaseSettings):
         env_file_with_env = Path(f"./infrastructure/core/envs/.{prefix}.{ENV}.env")
         env_file_generic = Path(f"./infrastructure/core/envs/.{prefix}.env")
 
-        if env_file_with_env.exists():
+        if ENV == "prod":
+            return cls()
+        elif env_file_with_env.exists():
             load_dotenv(dotenv_path=env_file_with_env, override=True)
         elif env_file_generic.exists():
             load_dotenv(dotenv_path=env_file_generic, override=True)
@@ -21,6 +23,3 @@ class BaseSettingsConfig(BaseSettings):
             raise FileNotFoundError(f"No .env file found for {prefix} with ENV '{ENV}'")
 
         return cls()
-
-    class Config:
-        env_file_encoding = "utf-8"
