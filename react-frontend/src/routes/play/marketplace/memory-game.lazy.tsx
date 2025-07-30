20c8f16f14009770f723d9d346f3ba85d56e72ae
marco <marcoadrianzambra@gmail.com>
2025-07-29 22:45:07 -0500

    style: Enhance Memory Game styles with gradients, shadows, and responsive adjustments

--- react-frontend/src/routes/play/marketplace/memory-game.lazy.tsx
+++ react-frontend/src/routes/play/marketplace/memory-game.lazy.tsx
@@ -3,7 +3,7 @@ import { MemoryGame } from '@/all-games/marketplace/memory-game';
 
 export const Route = createLazyFileRoute('/play/marketplace/memory-game')({
   component: () => (
-    <div className="min-h-full flex-grow bg-gray-800 ">
+    <div className="min-h-full flex-grow bg-gray- ">
       <MemoryGame />
     </div>
   ),
